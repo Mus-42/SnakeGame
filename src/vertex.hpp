abaa31829cf39242b58d2ce4d9f64ef34f398c80
_Mus <favmake@gmail.com>
2022-04-08 21:10:33 +0300

    build_shape implementation for rect class

--- src/vertex.hpp
+++ src/vertex.hpp
@@ -14,13 +14,21 @@ struct vertex {
     color col;
 };
 
-inline std::vector<vertex> build_shape(const rect& r, const irect& texture_rect = irect()) {
-    std::vector<vertex> ret(4);
-    //TODO add impl
+inline std::vector<vertex> build_shape(const rect& r, color fill_color, const irect& texture_rect = irect()) {//TODO pass tex size as arg to calc UV?
+    std::vector<vertex> ret(6, {{}, {}, fill_color});
+    //first triangle
+    ret[0].pos = r.a;
+    ret[1].pos = {r.a.x, r.b.y};
+    ret[2].pos = {r.b.x, r.a.y};
+    //second triangle
+    ret[3].pos = r.b;
+    ret[4].pos = {r.a.x, r.b.y};
+    ret[5].pos = {r.b.x, r.a.y};
+    //TODO add uv calculation
     return ret;
 }
 
-inline std::vector<vertex> build_shape(const circ& c, const irect& texture_rect = irect()) {
+inline std::vector<vertex> build_shape(const circ& c, color fill_color, const irect& texture_rect = irect()) {
     std::vector<vertex> ret(0);//TODO calc size
     //TODO add impl
     return ret;
